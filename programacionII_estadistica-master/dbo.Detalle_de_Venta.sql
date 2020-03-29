3cbaf2d123e549c92fb776313d7229c942e2b46b
Elba7014 <60446253+Elba7014@users.noreply.github.com>
2020-03-29 16:07:05 -0600

    Add files via upload

---
+++ programacionII_estadistica-master/dbo.Detalle_de_Venta.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[Detalle de Venta] (
+    [IdProductos]      INT         NOT NULL,
+    [Descripción]      NCHAR (100) NOT NULL,
+    [Unidades]         NCHAR (500) NOT NULL,
+    [Precio Unitario]  NCHAR (10)  NOT NULL,
+    [Fecha de comprar] DATE        NOT NULL,
+    [Precio total]     NCHAR (10)  NOT NULL,
+    [IdEmpleado]       INT         NOT NULL,
+    [IdFactura] INT NOT NULL, 
+    PRIMARY KEY CLUSTERED ([IdProductos]),
+    CONSTRAINT [Id_Empleado] FOREIGN KEY ([IdEmpleado]) REFERENCES [dbo].[Empleados] ([IdEmpleado]), 
+
+
+
+);
+
---
+++ programacionII_estadistica-master/dbo.Inventario.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Inventario] (
+    [IdInventario] INT         NOT NULL,
+    [IdProductos]  INT         NOT NULL,
+    [Unidades]     NCHAR (500) NOT NULL,
+    PRIMARY KEY CLUSTERED ([IdInventario] ASC),
+    CONSTRAINT [IdInventario] FOREIGN KEY ([IdInventario]) REFERENCES [dbo].[Inventario] ([IdInventario]), 
+    CONSTRAINT [idproductos] FOREIGN KEY ([IdProductos]) REFERENCES [dbo].[Detalle de Venta]([IdProductos])
+);
+
